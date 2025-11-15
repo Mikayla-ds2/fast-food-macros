0e298be4c3f554807fb06afe2c5897ca2c95c23a
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-11-15 03:53:43 -0500

    did some chat queries to enjoy this lovely font & learned i need to know about window functions

--- src/script.sql
+++ src/script.sql
@@ -65,3 +65,57 @@ where sugars < 15
 group by restaurant
 order by restaurantCount desc;
 -- trying to see how many times a restaurant is in a specific cluster --
+
+select * from fastfood
+
+select item_name, item_type, restaurant, calories, protein, price, (price / protein) as PPratio from fastfood
+where protein > 10
+order by PPratio desc;
+-- average protein to price ratio --
+select item_name, item_type, restaurant, calories, protein, price, (price / protein) as ppratio from fastfood
+where protein > 0 and price > 5.00 and not item_type = 'Drink'
+order by ppratio desc;
+
+select restaurant, avg(price / protein) as averageRatio from fastfood
+where protein > 0
+group by restaurant
+order by averageRatio desc;
+
+select item_type, restaurant, avg(calories) as averageCalories from fastfood
+group by item_type, restaurant
+order by averageCalories desc;
+
+-- notes for next query : average calories = 430 & average sugars = 5
+select * from fastfood
+where calories < 430 and sugars > 5
+order by sugars desc;
+
+select item_id, item_name, restaurant, protein, price from fastfood
+where protein > 25
+order by price asc
+limit 20;
+
+select * from fastfood
+where restaurant = 'Starbucks'
+order by protein desc;
+
+select item_name, item_type, restaurant, sugars, price from (select *, ROW_NUMBER()
+over (PARTITION BY restaurant
+order by price asc) as rn from fastfood where sugars > 5) ranked
+where rn <= 1;
+
+select * from fastfood
+order by sodium desc;
+-- to do this sort of query, need to use window functions; watch video on it later --
+select * from (select *, ROW_NUMBER() 
+over (PARTITION BY restaurant 
+order by sodium desc) as rn from fastfood) ranked
+where rn <= 1;
+
+select restaurant, protein, fats, item_name from fastfood
+where protein > 18 and fats < 21
+-- query notes : average protein = 18 & fats = 21
+
+select * from fastfood
+where restaurant = 'Taco Bell'
+order by fats asc;
